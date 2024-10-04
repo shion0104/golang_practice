db4ffab87bea640e778907d5b4257fb093a2ca40
shion0104 <zhiyinzhongze@gmail.com>
2024-10-04 13:40:07 +0900

    文字列について

---
+++ lesson5-3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Hello Golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+}
